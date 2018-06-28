205343f0343e0621a00884d5798121e47ac7a788
Yaswant Narayan <yoyaswant@gmail.com>
2018-06-29 00:58:21 +0530

    add preference injector

--- mobile/src/main/java/com/dscvit/android/nearlly/di/module/AppModule.kt
+++ mobile/src/main/java/com/dscvit/android/nearlly/di/module/AppModule.kt
@@ -2,9 +2,11 @@ package com.dscvit.android.nearlly.di.module
 
 import android.app.Application
 import android.arch.persistence.room.Room
+import android.content.Context
 import android.content.SharedPreferences
 import com.dscvit.android.nearlly.db.ChatDao
 import com.dscvit.android.nearlly.db.ChatDatabase
+import com.dscvit.android.nearlly.utils.Constants
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
@@ -17,16 +19,16 @@ class AppModule {
     @Singleton
     fun provideGron(): Gson = Gson()
 
-//    @Provides
-//    @Singleton
-//    fun providePreferences(application: Application): SharedPreferences { }
+    @Provides
+    @Singleton
+    fun providePreferences(application: Application): SharedPreferences = application.getSharedPreferences(Constants.PREF_KEY, Context.MODE_PRIVATE)
 
     @Provides
     @Singleton
     fun provideDatabase(application: Application): ChatDatabase = Room.databaseBuilder(
             application,
             ChatDatabase::class.java,
-            "chatdatabase.db"
+            Constants.DB_NAME
     ).build()
 
     @Provides
---
+++ mobile/src/main/java/com/dscvit/android/nearlly/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.dscvit.android.nearlly.utils
+
+object Constants {
+
+    val DB_NAME = "chatdatabase.db"
+    val PREF_KEY = "com.dscvit.android.nearlly.pref"
+    val PREF_USERNAME_KEY = "com.dscvit.android.nearlly.pref.username"
+}
